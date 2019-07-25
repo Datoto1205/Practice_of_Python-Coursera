1c8e373134d5f7d38c70d6a63f803c6b585261e2
Li Cheng-En <datotogood@icloud.com>
2019-07-25 13:04:22 +0800

    Solve the privacy problem of API Key.

--- Twitter/hidden.py
+++ Twitter/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "CNCEVU9TvGOG1HTtd6PEy55GP",
-            "consumer_secret": "4Zp00AfSRgNjLWvoIl6q0IHCB3GgE1amQo0CvjTFMrdHbIBkhp",
-            "token_key": "4120507334-rhhRUzRh8zMBDCU1zSGRxmSdWbHVmOlAHvZmvwp",
-            "token_secret": "tc6pR0ykNodm3nXAtPA0eBGgGmnLT7wmvHjIvsCMjVUt3"}
+    return {"consumer_key": "CNCEVU9Tv我不告訴你PEy55GP",
+            "consumer_secret": "4Zp00AfSRgNjL我不告訴你E1amQo0CvjTFMrdHbIBkhp",
+            "token_key": "4120507334-rhhR我不告訴你xmSdWbHVmOlAHvZmvwp",
+            "token_secret": "tc6pR0ykNodm3nXA我不告訴你T7wmvHjIvsCMjVUt3"}
