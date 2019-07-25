9228049cab235e66694c0b0925d86b96e95e68d1
Li Cheng-En <datotogood@icloud.com>
2019-07-25 12:56:26 +0800

    Add some academic materials related to the works of processing the data from Twitter

---
+++ Twitter/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "CNCEVU9TvGOG1HTtd6PEy55GP",
+            "consumer_secret": "4Zp00AfSRgNjLWvoIl6q0IHCB3GgE1amQo0CvjTFMrdHbIBkhp",
+            "token_key": "4120507334-rhhRUzRh8zMBDCU1zSGRxmSdWbHVmOlAHvZmvwp",
+            "token_secret": "tc6pR0ykNodm3nXAtPA0eBGgGmnLT7wmvHjIvsCMjVUt3"}
